4cd204c97d14c4f75c338c29449421c40ac5315c
Roland Braun <mail@rolandbraun.com>
2022-02-09 19:51:01 +0100

    Initial commit

---
+++ Index.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatiCsharp
+{
+    /// <summary>
+    /// A representation of the website's main index page
+    /// </summary>
+    public class Index
+    {
+    }
+}
--- Website.cs
+++ Website.cs
@@ -1,5 +1,26 @@
-﻿namespace StatiCsharp
+﻿using System.Globalization; // CultureInfo
+
+namespace StatiCsharp
 {
+
+    public interface IWebsite
+    {
+        /// The absolute url that the website will be hosted at.
+        string Url { get; }
+
+        /// The name of the website.
+        string Name { get; }
+
+        /// A description of the website.
+        string Description { get; }
+
+        /// The website's primary language
+        CultureInfo language { get; }
+
+        /// The website's favicon, if any.
+        //TODO
+        
+    }
     public class Website
     {
 
