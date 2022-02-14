7851b918654b993571ab1e9777d4994abd9a8011
Roland Braun <mail@rolandbraun.com>
2022-02-14 13:58:14 +0100

    Initial commit

---
+++ Sources/Interfaces/IHtmlComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatiCsharp
+{   
+    /// <summary>
+    /// Interface that all html-components must implement to ensure the output/render as a string containing html-code.
+    /// </summary>
+    internal interface IHtmlComponent
+    {
+        /// <summary>
+        /// Renders the html component as a string.
+        /// </summary>
+        /// <returns>A string containing the components html-code.</returns>
+        internal string Render();
+    }
+}
