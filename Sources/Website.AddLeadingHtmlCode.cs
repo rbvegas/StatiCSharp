7556b3458e7b6ba24795399c4d695a09e07b4e77
Roland Braun <mail@rolandbraun.com>
2022-02-20 09:51:15 +0100

    Added AddLeadingHtmlCode() to a partial class

---
+++ Sources/Website.AddLeadingHtmlCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatiCsharp.Interfaces;
+
+namespace StatiCsharp
+{
+    public partial class Website: IWebsite
+    {
+        /// <summary>
+        /// Add the leading (and trailing) html code to a site.
+        /// </summary>
+        /// <param name="website">The website, containing information for the head.</param>
+        /// <param name="context">The site where the html-code should be added.</param>
+        /// <param name="body">The body for the the, rendered by a HtmlFactory</param>
+        /// <returns>The content for a html-file as a string.</returns>
+        private string AddLeadingHtmlCode(IWebsite website, ISite context, string body)
+        {
+            StringBuilder siteBuilder = new StringBuilder();
+            siteBuilder.Append("<!doctype html>");
+            siteBuilder.Append($"<html lang=\"{website.Language.Name}\">");
+            siteBuilder.Append("<head>");
+            siteBuilder.Append("<meta charset=\"utf-8\">");
+            siteBuilder.Append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            siteBuilder.Append($"<title>{context.Title}</title>");
+            siteBuilder.Append($"<meta name=\"description\" content=\"{website.Description}\">");
+            siteBuilder.Append($"<link rel=\"stylesheet\" href=\"/styles.css\"");
+            siteBuilder.Append("</head>");
+            siteBuilder.Append("<body>");
+            siteBuilder.Append(body);
+            siteBuilder.Append("</body>");
+            siteBuilder.Append("</html>");
+
+            return siteBuilder.ToString();
+        }
+    }
+}
--- Sources/Website.cs
+++ Sources/Website.cs
@@ -131,33 +131,6 @@ namespace StatiCsharp
         }
 
         /// <summary>
-        /// Add the leading (and trailing) html code to a site.
-        /// </summary>
-        /// <param name="website">The website, containing information for the head.</param>
-        /// <param name="context">The site where the html-code should be added.</param>
-        /// <param name="body">The body for the the, rendered by a HtmlFactory</param>
-        /// <returns>The content for a html-file as a string.</returns>
-        private string AddLeadingHtmlCode(IWebsite website, ISite context, string body)
-        {
-            StringBuilder siteBuilder = new StringBuilder();
-            siteBuilder.Append("<!doctype html>");
-            siteBuilder.Append($"<html lang=\"{website.Language.Name}\">");
-            siteBuilder.Append("<head>");
-            siteBuilder.Append("<meta charset=\"utf-8\">");
-            siteBuilder.Append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            siteBuilder.Append($"<title>{context.Title}</title>");
-            siteBuilder.Append($"<meta name=\"description\" content=\"{website.Description}\">");
-            siteBuilder.Append($"<link rel=\"stylesheet\" href=\"/styles.css\"");
-            siteBuilder.Append("</head>");
-            siteBuilder.Append("<body>");
-            siteBuilder.Append(body);
-            siteBuilder.Append("</body>");
-            siteBuilder.Append("</html>");
-            
-            return siteBuilder.ToString();
-        }
-
-        /// <summary>
         /// Adds the given meta data to a site (index, page, section or item). If there is no field for a given entry, it's sciped.
         /// </summary>
         /// <param name="metaData">The meta data.</param>
