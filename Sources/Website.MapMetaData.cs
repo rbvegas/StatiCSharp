9f7ff4d98f2b2b00027afb1c78c031f237736bc0
Roland Braun <mail@rolandbraun.com>
2022-03-15 12:15:20 +0100

    Bugfix Title and description no longer contain <p>-tags and \n.

--- Sources/Website.MapMetaData.cs
+++ Sources/Website.MapMetaData.cs
@@ -17,12 +17,54 @@ namespace StatiCsharp
         /// <param name="site">The site where to add the meta data.</param>
         private void MapMetaData(Dictionary<string, string> metaData, ISite site)
         {
-            try { if (metaData["title"] is not null) { site.Title = Markdown.ToHtml(metaData["title"]); } } catch { }
-            try { if (metaData["description"] is not null) { site.Description = Markdown.ToHtml(metaData["description"]); } } catch { }
-            try { if (metaData["author"] is not null) { site.Author = metaData["author"]; } } catch { }
-            try { if (metaData["date"] is not null) { site.Date = DateOnly.Parse(metaData["date"]); } } catch { }
-            try { if (metaData["path"] is not null) { site.Path = metaData["path"]; } } catch { }
-            try { if (metaData["tags"] is not null) { site.Tags = metaData["tags"].Replace(" ", string.Empty).Split(',').ToList(); } } catch { }
+            // Markdown.ToHtml adds <p>-marks at the beginning and end of each value. This is sliced manually every time for now. Trim() removes \n at the end of the string.
+            try
+            {
+                if (metaData["title"] is not null)
+                {
+                    site.Title = Markdown.ToHtml(metaData["title"]).Replace("<p>", "").Replace("</p>", "").Trim();
+                }
+            } catch { }
+            
+            try
+            {
+                if (metaData["description"] is not null)
+                {
+                    site.Description = Markdown.ToHtml(metaData["description"]).Replace("<p>", "").Replace("</p>", "").Trim();
+                }
+            } catch { }
+
+            try
+            {
+                if (metaData["author"] is not null)
+                {
+                    site.Author = metaData["author"];
+                }
+            } catch { }
+
+            try
+            {
+                if (metaData["date"] is not null)
+                {
+                    site.Date = DateOnly.Parse(metaData["date"]);
+                }
+            } catch { }
+
+            try
+            {
+                if (metaData["path"] is not null)
+                {
+                    site.Path = metaData["path"];
+                }
+            } catch { }
+
+            try
+            {
+                if (metaData["tags"] is not null)
+                {
+                    site.Tags = metaData["tags"].Replace(" ", string.Empty).Split(',').ToList();
+                }
+            } catch { }
         }
     }
 }
