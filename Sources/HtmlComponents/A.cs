06e8dd813396abadcb2480e118306a6aa1ceebb5
Roland Braun <mail@rolandbraun.com>
2022-03-19 10:05:41 +0100

    Bugfix IHtmlComponents is now available via StatiCsharp.Interfaces

--- Sources/HtmlComponents/A.cs
+++ Sources/HtmlComponents/A.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StatiCsharp.Interfaces;
 
 namespace StatiCsharp.HtmlComponents
 {
--- Sources/HtmlComponents/HTML.cs
+++ Sources/HtmlComponents/HTML.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StatiCsharp.HtmlComponents;
+using StatiCsharp.Interfaces;
 
 namespace StatiCsharp.HtmlComponents
 {
