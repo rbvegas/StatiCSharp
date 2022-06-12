f706596af56deb46515f74d600d7bfd033846d69
Roland Braun <mail@rolandbraun.com>
2022-06-12 10:08:13 +0200

    Initial commit

---
+++ Sources/Website.CheckEnvironment.cs
@@ -0,0 +1,108 @@
+﻿using StatiCsharp.Interfaces;
+using StatiCsharp.Tools;
+using static System.Console;
+
+namespace StatiCsharp
+{
+    public partial class Website : IWebsite
+    {
+        /// <summary>
+        /// Checks if all nessessary folders exist and if StatiC# can read and write in this folders.
+        /// If a folder does not exists its created.
+        /// Failures are printed in the console.
+        /// </summary>
+        /// <returns>True, if the check is ok.</returns>
+        private bool CheckEnvironment()
+        {
+            if (!Directory.Exists(Output))
+            {
+                try
+                {
+                    Directory.CreateDirectory(Output);
+                }
+                catch
+                {
+                    WriteLine($"Your output directory does not exist. Trying to create it failed. Do you have read and write access to {Output} ?");
+                    return false;
+                }
+            }
+
+            try
+            {
+                DirectoryIsWritable(Output);
+            }
+            catch
+            {
+                WriteLine($"Trying to write to {Output} failed. Do you have read and write access?");
+                return false;
+            }
+
+
+            if (!Directory.Exists(Content))
+            {
+                try
+                {
+                    Directory.CreateDirectory(Content);
+                }
+                catch
+                {
+                    WriteLine($"Your content directory does not exist. Trying to create it failed. Do you have read and write access to {Content} ?");
+                    return false;
+                }
+            }
+
+            try
+            {
+                DirectoryIsWritable(Content);
+            }
+            catch
+            {
+                WriteLine($"Trying to write to {Content} failed. Do you have read and write access?");
+                return false;
+            }
+
+
+            if (!Directory.Exists(Resources))
+            {
+                try
+                {
+                    Directory.CreateDirectory(Resources);
+                }
+                catch
+                {
+                    WriteLine($"Your resources directory does not exist. Trying to create it failed. Do you have read and write access to {Resources} ?");
+                    return false;
+                }
+            }
+
+            try
+            {
+                DirectoryIsWritable(Resources);
+            }
+            catch
+            {
+                WriteLine($"Trying to write to {Resources} failed. Do you have read and write access?");
+                return false;
+            }
+
+            return true;
+
+            
+            void DirectoryIsWritable(string path)
+            {
+                try
+                {
+                    using (
+                        FileStream fs = File.Create(Path.Combine(path, Path.GetRandomFileName()), 1, FileOptions.DeleteOnClose)
+                        )
+                    { }
+                }
+                catch
+                {
+                    throw;
+                }
+
+            }
+        }
+    }
+}
