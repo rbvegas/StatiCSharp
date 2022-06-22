ff755b8356b5a710d66b0149e2e86c70ae7a5684
Roland Braun <mail@rolandbraun.com>
2022-06-22 11:44:56 +0200

    Initial commit

---
+++ Sources/HtmlComponents/HtmlElement.cs
@@ -0,0 +1,18 @@
+﻿using StatiCsharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatiCsharp.HtmlComponents
+{
+    public class HtmlElement : IHtmlComponent
+    {
+
+        public string Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
