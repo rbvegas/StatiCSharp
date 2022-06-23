c09fc91f5b2b01fc5b2d016ac6854fe7d43493f3
Roland Braun <mail@rolandbraun.com>
2022-06-23 13:55:40 +0200

    Update Added attribute target.

--- Sources/HtmlComponents/A.cs
+++ Sources/HtmlComponents/A.cs
@@ -47,9 +47,31 @@ namespace StatiCsharp.HtmlComponents
         /// <returns>this - the element itself.</returns>
         public A Href(string href)
         {
-            if (!Attributes.TryAdd("href", href))
+            Attributes["href"] = href;
+            return this;
+        }
+
+        /// <summary>
+        /// Where to display the linked URL.<br/><br/>
+        /// <para>
+        /// The following keywords have special meanings for where to load the URL:<br/>
+        /// "_self": the current browsing context. (Default)<br/>
+        /// "_blank": usually a new tab, but users can configure browsers to open a new window instead.<br/>
+        /// "_parent": the parent browsing context of the current one. If no parent, behaves as "_self".<br/>
+        /// "_top": the topmost browsing context (the "highest" context that's an ancestor of the current one).
+        /// If no ancestors, behaves as "_self".
+        /// </para>
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public A Target(string target)
+        {
+            string[] allowed = new string[] {"_self", "_blank", "_parent", "_top"};
+            var exists = Array.Exists(allowed, element => element == target);
+            if (exists)
             {
-                Attributes["href"] = href;
+                Attributes["target"] = target;
+                return this;
             }
             return this;
         }
