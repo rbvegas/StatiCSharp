9dff8a1d3482bda0876006d1b97918a4234d338a
Roland Braun <mail@rolandbraun.com>
2022-07-02 15:50:07 +0200

    Update Removed access modifier.

--- Sources/Interfaces/IHtmlFactory.cs
+++ Sources/Interfaces/IHtmlFactory.cs
@@ -8,47 +8,47 @@
         /// <summary>
         /// Path to the corresponding resources. All files within this directory will be copied to the root directory of the output.
         /// </summary>
-        public string ResourcesPath { get; }
+        string ResourcesPath { get; }
 
         /// <summary>
         /// The website the theme is used for. So that the theme can access additional information.<br/><br/>
         /// StatiC# will inject the current website into this property.
         /// </summary>
-        public IWebsite? Website { get; set; }
+        IWebsite? Website { get; set; }
 
         /// <summary>
         /// Creates html-code for inside the &lt;head&gt;&lt;/head&gt;-tag. This code is added to all sites.
         /// </summary>
         /// <returns>A string containing the html-code.</returns>
-        public string MakeHeadHtml();
+        string MakeHeadHtml();
 
         /// <summary>
         /// Method that returns the html-code for the index site.
         /// </summary>
         /// /// <param name="website">The websites index to render.</param>
         /// <returns>A string containing the html-code.</returns>
-        public string MakeIndexHtml(IWebsite website);
+        string MakeIndexHtml(IWebsite website);
 
         /// <summary>
         /// Method that returns the html-code for a page (not section or item).
         /// </summary>
         /// <param name="page">The page to render.</param>
         /// <returns>A string containing the html-code.</returns>
-        public string MakePageHtml(IPage page);
+        string MakePageHtml(IPage page);
 
         /// <summary>
         /// Method that returns the html-code for a section site.
         /// </summary>
         /// <param name="section">The section to render.</param>
         /// <returns>A string containing the html-code.</returns>
-        public string MakeSectionHtml(ISection section);
+        string MakeSectionHtml(ISection section);
 
         /// <summary>
         /// Method that returns the html-code for an item site.
         /// </summary>
         /// <param name="item">The item to render.</param>
         /// <returns>A string containing the html-code.</returns>
-        public string MakeItemHtml(IItem item);
+        string MakeItemHtml(IItem item);
 
         /// <summary>
         /// Method that returns the html-code for the taglist site.
@@ -56,6 +56,6 @@
         /// <param name="items">A list of the items that include this tag.</param>
         /// <param name="tag">The name of the tag.</param>
         /// <returns>A string containing the html-code.</returns>
-        public string MakeTagListHtml(List<IItem> items, string tag);
+        string MakeTagListHtml(List<IItem> items, string tag);
     }
 }
