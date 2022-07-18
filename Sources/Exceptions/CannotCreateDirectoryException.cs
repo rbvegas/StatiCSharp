7b0512f68d5dad51129c3359cc7e566879f2c6e9
Roland Braun <dev@rolandbraun.com>
2022-07-18 10:40:50 +0200

    Initial commit

---
+++ Sources/Exceptions/CannotCreateDirectoryException.cs
@@ -0,0 +1,19 @@
+﻿namespace StatiCSharp.Exceptions;
+
+internal class CannotCreateDirectoryException : Exception
+{
+    public CannotCreateDirectoryException()
+    {
+
+    }
+    
+    public CannotCreateDirectoryException(string message) : base(message)
+    {
+
+    }
+
+    public CannotCreateDirectoryException(string message, Exception inner) : base(message, inner)
+    {
+
+    }
+}
---
+++ Sources/Exceptions/DirectoryNotWriteableException.cs
@@ -0,0 +1,19 @@
+﻿namespace StatiCSharp.Exceptions;
+
+internal class DirectoryNotWriteableException : Exception
+{
+    public DirectoryNotWriteableException()
+    {
+
+    }
+
+    public DirectoryNotWriteableException(string message) : base(message)
+    {
+
+    }
+
+    public DirectoryNotWriteableException(string message, Exception inner) : base(message, inner)
+    {
+
+    }
+}
