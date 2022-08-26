cc6574406c727b69f7c66b9d8079a08415ccf9af
Roland Braun <dev@rolandbraun.com>
2022-08-26 15:34:30 +0200

    Update Added async version of the method.

--- Sources/WebsiteManager.MakeSections.cs
+++ Sources/WebsiteManager.MakeSections.cs
@@ -26,4 +26,37 @@ public partial class WebsiteManager : IWebsiteManager
             PathDirectory.Add(path);
         }
     }
+
+    /// <summary>
+    /// Asynchronously creates and writes the sections (not pages or items) of the website.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous sections generating operation.</returns>
+    private async Task MakeSectionsAsync()
+    {
+        List<Task> tasks = new List<Task>();
+
+        foreach (ISection site in Website.Sections)
+        {
+            tasks.Add(WriteSection(site));
+        }
+
+        await Task.WhenAll(tasks);
+
+        async Task WriteSection(ISection site)
+        {
+            string body = HtmlFactory.MakeSectionHtml(site);
+            string head = HtmlFactory.MakeHeadHtml();
+            string page = AddLeadingHtmlCode(Website, site, head, body);
+            string path = Directory.CreateDirectory(Path.Combine(Output, site.SectionName)).ToString();
+
+            if (PathDirectory.Contains(path))
+            {
+                Console.WriteLine($"WARNING: The path {path} is allready in use. Change the path in meta data to avoid duplicates.");
+            }
+
+            await WriteFileAsync(path: path, filename: "index.html", content: page, gitMode: GitMode);
+
+            PathDirectory.Add(path);
+        }
+    }
 }
--- Sources/WebsiteManager.MakeTagLists.cs
+++ Sources/WebsiteManager.MakeTagLists.cs
@@ -55,4 +55,65 @@ public partial class WebsiteManager : IWebsiteManager
             PathDirectory.Add(path);
         }
     }
+
+    private async Task MakeTagListsAsync()
+    {
+        // Collect all available tags
+        List<string> tags = new List<string>();
+        foreach (ISection currentSection in Website.Sections)
+        {
+            foreach (IItem currentItem in currentSection.Items)
+            {
+                foreach (string tag in currentItem.Tags)
+                {
+                    // Check if tag is already in list. If not, add it
+                    if (!tags.Contains(tag)) { tags.Add(tag); }
+                }
+            }
+        }
+
+        List<Task> tasks = new List<Task>();
+        
+        foreach (string tag in tags)
+        {
+            tasks.Add(WriteTagList(tag));
+        }
+
+        await Task.WhenAll(tasks);
+
+        async Task WriteTagList(string tag)
+        {
+            List<IItem> itemsWithCurrentTag = new();
+            // Collect all items with the current tag
+            foreach (ISection currentSection in Website.Sections)
+            {
+                foreach (IItem item in currentSection.Items)
+                {
+                    if (item.Tags.Contains(tag))
+                    {
+                        itemsWithCurrentTag.Add(item);
+                    }
+                }
+            }
+
+            // Write tags sites to files
+            IItem tagPage = new Item();
+            tagPage.Title = $"{tag} | {Website.Name}";
+            string body = HtmlFactory.MakeTagListHtml(itemsWithCurrentTag, tag);
+            string head = HtmlFactory.MakeHeadHtml();
+            string page = AddLeadingHtmlCode(Website, tagPage, head, body);
+
+            // Create directory, if it does not excist
+            string path = Directory.CreateDirectory(Path.Combine(Output, "tag", tag)).ToString();
+
+            if (PathDirectory.Contains(path))
+            {
+                Console.WriteLine($"WARNING: The path {path} is allready in use. Change the path in meta data to avoid duplicates.");
+            }
+
+            await WriteFileAsync(path: path, filename: "index.html", content: page, gitMode: GitMode);
+
+            PathDirectory.Add(path);
+        }
+    }
 }
