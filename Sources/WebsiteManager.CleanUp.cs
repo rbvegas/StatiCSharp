dbeaa0fe5bf84e8fbade817b2fb872b106f7ac44
Roland Braun <dev@rolandbraun.com>
2022-08-27 16:52:40 +0200

    Update Added async version.

--- Sources/WebsiteManager.CleanUp.cs
+++ Sources/WebsiteManager.CleanUp.cs
@@ -43,4 +43,44 @@ public partial class WebsiteManager : IWebsiteManager
             }
         }
     }
+
+    private async Task CleanUpAsync()
+    {
+        string[] directories = Directory.GetDirectories(Output);
+        foreach (string directory in directories)
+        {
+            await cleanUpDirectory(directory);
+        }
+
+
+        async Task cleanUpDirectory(string dir)
+        {
+            // check if there are folders that are not in _pathDirectory
+            string[] currentDirectories = Directory.GetDirectories(dir);
+
+            foreach (string directory in currentDirectories)
+            {
+                if (!PathDirectory.Contains(directory))
+                {
+                    // Delete only files named index.html. Other files can be resources!
+                    if (File.Exists(Path.Combine(directory, "index.html")))
+                    {
+                        File.Delete(Path.Combine(directory, "index.html"));
+                    }
+
+                    if (Directory.GetDirectories(directory).Length == 0 && Directory.GetFiles(directory).Length == 0)
+                    {
+                        Directory.Delete(directory);
+                    }
+                    else
+                    {
+                        foreach (string subdir in Directory.GetDirectories(directory))
+                        {
+                            await cleanUpDirectory(subdir);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- Sources/WebsiteManager.CopyAll.cs
+++ Sources/WebsiteManager.CopyAll.cs
@@ -45,4 +45,41 @@ public partial class WebsiteManager : IWebsiteManager
             }
         }
     }
+
+
+    private async Task CopyAllAsync(string sourceDir, string destinationDir)
+    {
+        // https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+
+
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath, true);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (true)
+        {
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                await CopyAllAsync(subDir.FullName, newDestinationDir);
+            }
+        }
+    }
 }
