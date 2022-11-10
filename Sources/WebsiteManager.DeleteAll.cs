6509cb78d94b2c19e1d6e7a9bfc097d402d40079
Roland Braun <dev@rolandbraun.com>
2022-11-10 16:42:03 +0100

    Refactoring Removed async verion of the method, as no await was possible.

--- Sources/WebsiteManager.DeleteAll.cs
+++ Sources/WebsiteManager.DeleteAll.cs
@@ -7,28 +7,9 @@ public partial class WebsiteManager : IWebsiteManager
     /// <summary>
     /// Deletes all directories and files within the given path, without deleting the path folder itself.
     /// </summary>
-    /// <param name="path">Path of the directory.</param>
-    private void DeleteAll(string path)
-    {
-        DirectoryInfo directory = new DirectoryInfo(path);
-
-        foreach (FileInfo file in directory.GetFiles())
-        {
-            file.Delete();
-        }
-
-        foreach (DirectoryInfo dir in directory.GetDirectories())
-        {
-            dir.Delete(true);
-        }
-    }
-
-    /// <summary>
-    /// Asynchronously deletes all directories and files within the given path, without deleting the path folder itself.
-    /// </summary>
     /// <param name="path"></param>
-    /// <returns>A task that represents the asynchronous deleting operation.</returns>
-    private async Task DeleteAllAsync(string path)
+    /// <returns>void</returns>
+    private void DeleteAll(string path)
     {
         DirectoryInfo directory = new DirectoryInfo(path);
 
