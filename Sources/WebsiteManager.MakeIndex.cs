60ba290de2e9c3d2cb4af2d03add213007c24b97
Roland Braun <dev@rolandbraun.com>
2022-11-19 15:10:45 +0100

    Update Now using explicit usings instead of global usings. Updated spelling in the documentation.

--- Sources/WebsiteManager.MakeIndex.cs
+++ Sources/WebsiteManager.MakeIndex.cs
@@ -1,19 +1,20 @@
 ﻿using StatiCSharp.Interfaces;
+using System.Threading.Tasks;
 
 namespace StatiCSharp;
 
 public partial class WebsiteManager : IWebsiteManager
 {
     /// <summary>
-    /// Asynchronously creates and writes the index (homepage) of the website.
+    /// Asynchronous creates and writes the index (homepage) of the website.
     /// </summary>
     /// <returns>A <see cref="Task"/> that represents the asynchronous index generating operation.</returns>
     private async Task MakeIndexAsync()
     {
-        string body = HtmlFactory.MakeIndexHtml(Website.Index);
-        string head = HtmlFactory.MakeHeadHtml();
-        string index = AddLeadingHtmlCode(Website, Website.Index, head, body);
-        await WriteFileAsync(Output, "index.html", index, gitMode: GitMode);
-        PathDirectory.Add(Output);
+            string body = HtmlFactory.MakeIndexHtml(Website.Index);
+            string head = HtmlFactory.MakeHeadHtml();
+            string index = AddLeadingHtmlCode(Website, Website.Index, head, body);
+            await WriteFileAsync(Output, "index.html", index, gitMode: GitMode);
+            PathDirectory.Add(Output);
+        }
     }
-}
