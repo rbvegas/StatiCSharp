214b59bc6fe5c0e110f6fa2112e4c07c57c24458
Roland Braun <dev@rolandbraun.com>
2023-01-15 12:08:21 +0100

    Initial commit

---
+++ Sources/Interfaces/IHtmlBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace StatiCSharp.Interfaces;
+
+/// <summary>
+/// Responsible for creating valid HTML from markdown strings.
+/// </summary>
+public interface IHtmlBuilder
+{
+    /// <summary>
+    /// Content that is added to the header of every site. This content is needed to get the resulting HTML work.
+    /// </summary>
+    string AdditionalHeaderContent { get; }
+
+    /// <summary>
+    /// Add a parser to the end of the pipeline.
+    /// </summary>
+    /// <param name="parser"></param>
+    void AddToPipeline(IPipelineParser parser);
+
+    /// <summary>
+    /// Generates HTML from the input parameter. The predefined parser pipeline is used.
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    string ToHtml(string content);
+}
